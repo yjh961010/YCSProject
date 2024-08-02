9b8e0dd562f37a164ec711f7f2cb06e10ff11ad0
Heeseung- <soaorsith2@naver.com>
2024-08-02 10:13:04 +0900

    [수정: 리퀘스트매핑 추가]

--- src/main/java/com/example/neoheulge/notice/web/NoticeController.java
+++ src/main/java/com/example/neoheulge/notice/web/NoticeController.java
@@ -2,8 +2,10 @@ package com.example.neoheulge.notice.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/notice")
 public class NoticeController {
     @GetMapping("/noticeboard.do")
     public String noticeboard() { return "notice/noticeboard"; }
