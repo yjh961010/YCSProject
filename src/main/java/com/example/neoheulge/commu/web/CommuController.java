29bfdb96d8d9d85d12bb744f6a86df695b87415c
leejaehong <woqhr53@naver.com>
2024-08-02 10:22:38 +0900

    Revert "[수정][컨트롤러,헤더][jsp경로 이런식으로 패키지이름 붙여서 해주세요]"
    This reverts commit 1c37e72ab733ca51b1a8de7899e2c75f76aa52e3.

--- src/main/java/com/example/neoheulge/commu/web/CommuController.java
+++ src/main/java/com/example/neoheulge/commu/web/CommuController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/commu")
 public class CommuController {
 	@GetMapping("/commuList.do")
 	public String commuList() {
--- src/main/java/com/example/neoheulge/member/web/MemberController.java
+++ src/main/java/com/example/neoheulge/member/web/MemberController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/member")
 public class MemberController {
 	@GetMapping("/login.do")
 	public String login() {
