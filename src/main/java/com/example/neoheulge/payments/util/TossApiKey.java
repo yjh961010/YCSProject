37d53d3987e6a0795db4de64882fd444ff40c721
seven2762 <seven2762@gmail.com>
2024-08-05 18:15:22 +0900

    [추가][dto,entity,config][dto,entity,config 수정 및 추가]

--- src/main/java/com/example/neoheulge/payments/util/TossApiKey.java
+++
@@ -1,8 +0,0 @@
-package com.example.neoheulge.payments.util;
-
-public class TossApiKey {
-
-    private final String cliKey = "test_ck_6bJXmgo28e1pJj0EzE1A8LAnGKWx";
-    private final String secretKey = "test_ck_6bJXmgo28e1pJj0EzE1A8LAnGKWx";
-
-}
