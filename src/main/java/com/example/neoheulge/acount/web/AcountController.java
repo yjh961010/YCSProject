0e7410820c21c4d0a01b5b8913acd234b8f2b96b
GYWNS7967\gywns <gywns7967@gmail.com>
2024-08-22 13:52:44 +0900

    [수정]

--- src/main/java/com/example/neoheulge/acount/web/AcountController.java
+++ src/main/java/com/example/neoheulge/acount/web/AcountController.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,8 +34,10 @@ public class AcountController {
 
 	@PostMapping("/insertNeacount.do")
 	public String addAccount(NeAcountDTO dto) {
+    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    	String username = authentication.getName();
 		acountService.addAccount(dto);
-		return "member/myPage="+dto.getMember_id();
+		return "redirect:/member/myPage.do?user="+username;
 	}
 
 	@GetMapping("/insertNeacountform.do")
